71a19e6d89e45392e6e822144e814957bf3a2fbb
Monalisa Correa de Melo <mel@certi.org.br>
2021-03-07 20:51:56 -0300

    Add filter config to Product

--- tech_start/product/views.py
+++ tech_start/product/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from .models import Product, Category
 from rest_framework import viewsets
 from .serializers import ProductSerializer, CategorySerializer
+from django_filters.rest_framework import FilterSet
+from rest_framework import filters, generics
 
-
+class DynamicSearchFilter(FilterSet):
+    class Meta:
+        model = Product
+        fields = {
+            'name': ['exact', 'contains'],
+            'description': ['exact', 'contains'],
+            'price': ['exact', 'lt', 'gt'],
+            'categories': ['exact'],
+        }
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    filter_class = DynamicSearchFilter
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
--- tech_start/tech_start/settings.py
+++ tech_start/tech_start/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'marketplace',
     'product',
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',
+    # 'rest_framework.filters.SearchFilter',
+    ]
+}
